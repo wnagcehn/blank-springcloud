b0dcd65f8c6b8e8e77f380ec9e2674777290a23d
iwnagcehn <iwnagcehn@outlook.com>
2019-04-27 01:28:13 +0800

    Initial commit

---
+++ blank-springcloud-consumer/src/main/java/com/comic/blankspringcloudconsumer/BlankSpringcloudConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.comic.blankspringcloudconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankSpringcloudConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankSpringcloudConsumerApplication.class, args);
+	}
+
+}
---
+++ blank-springcloud-producer/src/main/java/com/comic/blankspringcloudproducer/BlankSpringcloudProducerApplication.java
@@ -0,0 +1,14 @@
+package com.comic.blankspringcloudproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BlankSpringcloudProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankSpringcloudProducerApplication.class, args);
+	}
+
+}
